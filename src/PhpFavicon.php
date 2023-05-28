38f762d5f871af55bc5c4b9fc555bb6c080717c8
Arrtist <av.arrtist@gmail.com>
2023-05-28 13:06:54 +0400

    PhpFavicon class

---
+++ src/PhpFavicon.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Beaubus\PhpFavicon;
+
+class PhpFavicon
+{
+    /**
+     * Render favicon tags to the string
+     */
+    public function faviconTags(): string
+    {
+        return <<<TAGS
+            <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>☘️</text></svg>">
+        TAGS;
+    }
+}
