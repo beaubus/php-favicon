9732d23cd279f2327cc29e51aa914d694bb70e0d
Arrtist <av.arrtist@gmail.com>
2023-05-29 13:45:03 +0400

    tested

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # BEAUBUS PHP Favicon package
 
 Package of [110+ free favicons](https://favicons.beaubus.com) for your vanilla php project (12 sizes for each favicon). 
+
+## Installation
+```shell
+composer require beaubus/php-favicon
+```
+
+## Usage
+```php
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+$favicon = new \Beaubus\PhpFavicon('/path-to-the/public');
+$favicon->init(
+    'pocket watch', // Favicon name 
+     true           // Check if the favicon has changed. Set it to 'false' once you have chosen and tested your favicon.
+ ); 
+
+echo $favicon->tags();
+```
--- src/PhpFavicon.php
+++ src/PhpFavicon.php
@@ -9,23 +9,36 @@ class PhpFavicon
     public function __construct(string $public_path = '') {
         $this->public_path = trim($public_path);
 
+        if (!is_dir($this->public_path)) mkdir($this->public_path);
+
         // add trailing slash to the end
         if (!empty($this->public_path) && !str_ends_with($this->public_path, '/')) $this->public_path .= '/';
     }
 
-    public function setup()
+    public function init($favicon_name = 'pocket watch', bool $check_for_name_updates = true)
     {
-        $favicon = $this->getFavicon('Light bulb');
+        if (!$check_for_name_updates) return;   // favicon is installed
+        if (!$this->isFaviconChanged($favicon_name)) return; // favicon is not changed
+
+        $favicon = $this->getFavicon($favicon_name);
+        if (!isset($favicon['name'])) return;
+
         $this->extractFavicon($favicon);
     }
 
     /**
      * Render favicon tags to the string
      */
-    public function faviconTags(): string
+    public function tags(): string
     {
         return <<<TAGS
-            <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>☘️</text></svg>">
+            <link rel="apple-touch-icon" sizes="180x180" href="/beaubus-favicon/apple-touch-icon.png">
+            <link rel="icon" type="image/png" sizes="32x32" href="/beaubus-favicon/favicon-32x32.png">
+            <link rel="icon" type="image/png" sizes="16x16" href="/beaubus-favicon/favicon-16x16.png">
+            <link rel="manifest" href="/beaubus-favicon/site.webmanifest">
+            <link rel="mask-icon" href="/beaubus-favicon/safari-pinned-tab.svg" color="#5bbad5">
+            <meta name="msapplication-TileColor" content="#2d89ef">
+            <meta name="theme-color" content="#ffffff">
         TAGS;
     }
 
@@ -33,12 +46,10 @@ class PhpFavicon
     {
         // make beaubus_favicon/ folder
         $path = $this->public_path . 'beaubus_favicon/';
-        mkdir($path);
+        if (!is_dir($path)) mkdir($path);
 
         // download zip file into tmp folder
         $tmp_file = $path . 'tmp.zip';
-        echo "copy from: " . $favicon['zip'] . PHP_EOL;
-        echo "copy to: " . $tmp_file . PHP_EOL;
         copy($favicon['zip'], $tmp_file);
 
         // Unzip into tmp folder
@@ -58,16 +69,38 @@ class PhpFavicon
         // replace paths in browserconfig
         $browserconfig_path = $path . 'browserconfig.xml';
         $browserconfig = file_get_contents($browserconfig_path);
-        $browserconfig = preg_replace('/src=\"\//', 'src="/beaubus-favicons/', $browserconfig);
+        $browserconfig = preg_replace('/src=\"\//', 'src="/beaubus-favicon/', $browserconfig);
         file_put_contents($browserconfig_path, $browserconfig);
 
         // replace paths in webmanifest
         $webmanifest_path = $path . 'site.webmanifest';
         $webmanifest = file_get_contents($webmanifest_path);
-        $webmanifest = preg_replace('/\"src\": \"\//', '"src": "/beaubus-favicons/', $webmanifest);
+        $webmanifest = preg_replace('/\"src\": \"\//', '"src": "/beaubus-favicon/', $webmanifest);
         file_put_contents($webmanifest_path, $webmanifest);
+
+        // save the favicon name
+        $cached_name_file = $path . 'installed.txt';
+        file_put_contents($cached_name_file, $favicon['name']);
     }
 
+    /**
+     * Open beaubus_favicon/installed.txt and look if name is the same as $favicon_name
+     * @param string $favicon_name Favicon name, specified by user. Example: 'pocket watch'.
+     * @return bool
+     */
+    private function isFaviconChanged(string $favicon_name): bool
+    {
+        $cached_name_file = $this->public_path . 'beaubus_favicon/installed.txt';
+        if (!file_exists($cached_name_file)) return true;
+        $cached_name = file_get_contents($cached_name_file);
+
+        return !(strtolower($favicon_name) === strtolower($cached_name));
+    }
+
+    /**
+     * @param $name
+     * @return array|null <{name, zip, svg}>
+     */
     private function getFavicon($name): array|null
     {
         $json = file_get_contents('https://favicons.beaubus.com/favicons.json');
